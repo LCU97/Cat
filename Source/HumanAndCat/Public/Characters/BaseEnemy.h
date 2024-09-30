d7a6e6ee2c56f6f310c05743cb2422fdd87c3818
LCU97 <saparationlee@gmail.com>
2024-09-30 21:22:01 +0900

    09-30
    basemonster

---
+++ Source/HumanAndCat/Public/Characters/BaseEnemy.h
@@ -0,0 +1,50 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "BaseEnemy.generated.h"
+
+
+USTRUCT(BlueprintType,Blueprintable)
+struct FEnemyStat
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float Hp;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float Atk;
+	
+};
+
+UCLASS()
+class HUMANANDCAT_API ABaseEnemy : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	ABaseEnemy();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+public:
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FEnemyStat EnemyStat;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	bool bEnableHit = true;
+};
